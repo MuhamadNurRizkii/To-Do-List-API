4492e92aa3ccdbf0a8f7092b80c7b6a5243f6467
Muhamad Nur Rizki <farianhab123@gmail.com>
2025-04-05 20:55:01 +1100

    menginstall module cors

--- api/server.js
+++ api/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(express.json());
 
 const Todo = [
